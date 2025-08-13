9efae389d525415d7cbac35792204e69e7e00a2c
r-kolla <revanthkolla2@gmail.com>
2025-08-13 12:12:54 +0530

    serializer

--- backend/meals/serializers.py
+++ backend/meals/serializers.py
@@ -2,9 +2,16 @@ from rest_framework import serializers
 from .models import Meal
 
 class MealSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Meal
         fields = '__all__'
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url if hasattr(obj.image, 'url') else obj.image
+        return None
 
 
 
